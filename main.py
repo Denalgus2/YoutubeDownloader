02df209e8e795181b662e7bcf0a8da9d10ba4705
Denis Gusev <Denis.ale.gusev@gmail.com>
2024-02-03 16:55:19 +0100

    Fixed video file clip and audio file clip

---
+++ main.py
@@ -0,0 +1,39 @@
+from pytube import YouTube
+import os
+from moviepy.editor import *
+
+# Create the directory if it doesn't exist
+output_directory = './clips'
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+# YouTube video URL
+video_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+# Download video and audio
+yt = YouTube(video_url)
+
+
+video_stream = yt.streams.order_by('resolution').desc().first()
+audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc().first()
+
+print(video_stream)
+
+# Download video and audio
+#video_stream.download(output_directory, filename='video.mp4')
+#audio.download(output_directory, filename='audio.mp4')
+
+
+#merge audio and video
+
+while True:
+    if os.path.exists(output_directory + '/video.mp4') and os.path.exists(output_directory + '/audio.mp4'):
+        break
+videoclip = VideoFileClip(output_directory + "/video.mp4")
+audioclip = AudioFileClip(output_directory + "/audio.mp4")
+
+print(videoclip)
+
+new_audioclip = CompositeAudioClip([audioclip])
+videoclip.audio = new_audioclip
+videoclip.write_videofile("Final.mp4")
